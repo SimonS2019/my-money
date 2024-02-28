14e71f3858a911617af89c6fa2b5758b9cab3b4c
SimonS2019 <shentiexiong@gmail.com>
2024-02-28 14:56:27 +1300

    117 Add Firebase configuration

---
+++ src/firebase/config.js
@@ -0,0 +1,19 @@
+import firebase from 'firebase/app'
+import 'firebase/firestore'
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCWetvFLHoyvyEpcUhE5VeG4ORJ_AwMJwc",
+  authDomain: "my-money-6d1d0.firebaseapp.com",
+  projectId: "my-money-6d1d0",
+  storageBucket: "my-money-6d1d0.appspot.com",
+  messagingSenderId: "191569265514",
+  appId: "1:191569265514:web:e5091263b4be35d93edb13"
+};
+
+// init firebase
+firebase.initializeApp(firebaseConfig)
+
+// init services
+const projectFirestore = firebase.firestore()
+
+export { projectFirestore }
