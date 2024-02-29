7dfba0bbabe300756813ad30bad27b55fa2a03c5
SimonS2019 <shentiexiong@gmail.com>
2024-03-01 12:04:41 +1300

    139 Refactor useCollection hook to support query parameters

--- src/hooks/useCollection.js
+++ src/hooks/useCollection.js
@@ -1,32 +1,43 @@
-import { useEffect, useState } from "react"
-import { projectFirestore } from "../firebase/config"
+import { useEffect, useState, useRef } from "react";
+import { projectFirestore } from "../firebase/config";
 
-export const useCollection = (collection) => {
-  const [documents, setDocuments] = useState(null)
-  const [error, setError] = useState(null)
+export const useCollection = (collection, _query) => {
+  const [documents, setDocuments] = useState(null);
+  const [error, setError] = useState(null);
 
+  // if we don't use a ref --> infinite loop in useEffect
+  // _query is an array and is "different" on every function call
+  const query = useRef(_query).current;
+  console.log(_query);
+console.log(query);
   useEffect(() => {
-    let ref = projectFirestore.collection(collection)
+    let ref = projectFirestore.collection(collection);
 
-    const unsubscribe = ref.onSnapshot(snapshot => {
-      let results = []
-      snapshot.docs.forEach(doc => {
-        console.log(doc)
-        results.push({...doc.data(), id: doc.id})
-      });
-      
-      // update state
-      setDocuments(results)
-      setError(null)
-    }, error => {
-      console.log(error)
-      setError('could not fetch the data')
-    })
+    if (query) {
+      ref = ref.where(...query);
+    }
+
+    const unsubscribe = ref.onSnapshot(
+      (snapshot) => {
+        let results = [];
+        snapshot.docs.forEach((doc) => {
+          console.log(doc);
+          results.push({ ...doc.data(), id: doc.id });
+        });
+
+        // update state
+        setDocuments(results);
+        setError(null);
+      },
+      (error) => {
+        console.log(error);
+        setError("could not fetch the data");
+      }
+    );
 
     // unsubscribe on unmount
-    return () => unsubscribe()
+    return () => unsubscribe();
+  }, [collection, query]);
 
-  }, [collection])
-
-  return { documents, error }
-}
+  return { documents, error };
+};
