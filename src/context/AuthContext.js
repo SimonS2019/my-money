b84247a876054144a5780379a46ba51b204b54be
SimonS2019 <shentiexiong@gmail.com>
2024-02-29 15:21:49 +1300

    127 Refactor authentication code and fix login functionality

--- src/context/AuthContext.js
+++ src/context/AuthContext.js
@@ -1,28 +1,28 @@
-import { createContext, useReducer } from "react";
+import { createContext, useReducer } from 'react'
 
-export const AuthContext = createContext();
+export const AuthContext = createContext()
 
 export const authReducer = (state, action) => {
   switch (action.type) {
-    case "LOGIN":
-      return { ...state, user: action.payload };
-
-    case "LOGOUT":
-      return { ...state, user: null };
+    case 'LOGIN':
+      return { ...state, user: action.payload }
+    case 'LOGOUT':
+      return { ...state, user: null }
     default:
-      return state;
+      return state
   }
-};
+}
 
 export const AuthContextProvider = ({ children }) => {
-  const [state, dispatch] = useReducer(authReducer, {
-    user: null,
-  });
-  console.log("AuthContext state:", state);
-
+  const [state, dispatch] = useReducer(authReducer, { 
+    user: null 
+  })
+  console.log('AuthContext state:', state)
+  
   return (
     <AuthContext.Provider value={{ ...state, dispatch }}>
-      {children}
+      { children }
     </AuthContext.Provider>
-  );
-};
+  )
+
+}
